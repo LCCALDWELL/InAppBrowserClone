ca0bdb226f39f8611c02e30ba74e6a5ad11943de
LCCALDWELL <31364252+LCCALDWELL@users.noreply.github.com>
2026-02-27 12:58:28 +1000

    Update defaults.d.ts

--- dist/defaults.d.ts
+++ dist/defaults.d.ts
@@ -1,5 +1,8 @@
-import { AndroidSystemBrowserOptions, SystemBrowserOptions, iOSSystemBrowserOptions  } from './definitions';
+import { AndroidSystemBrowserOptions, AndroidWebViewOptions, SystemBrowserOptions, WebViewOptions, iOSSystemBrowserOptions, iOSWebViewOptions } from './definitions';
 
+export declare const DefaultAndroidWebViewOptions: AndroidWebViewOptions;
+export declare const DefaultiOSWebViewOptions: iOSWebViewOptions;
+export declare const DefaultWebViewOptions: WebViewOptions;
 export declare const DefaultiOSSystemBrowserOptions: iOSSystemBrowserOptions;
 export declare const DefaultAndroidSystemBrowserOptions: AndroidSystemBrowserOptions;
 export declare const DefaultSystemBrowserOptions: SystemBrowserOptions;
